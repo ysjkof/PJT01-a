5bd0bef4862d22046e661661bc80cffe7639bd30
seongjin <ysjkof@hanmail.net>
2021-03-09 21:33:13 +0900

    갱신

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+const modalOpen = document.querySelector('.modal-open');
+const modalClose = document.querySelector('.modal-close');
+const modalWindow = document.querySelector('.modal-window');
+
+function closeModal(){
+  modalWindow.classList.add('hidden')
+};
+
+function openModal(){
+  modalWindow.classList.remove('hidden')
+};
+
+modalClose.addEventListener('click', closeModal);
+
+modalOpen.addEventListener('click', openModal);
